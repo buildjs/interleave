08400a6c8f700bbacc18757478a092db2dcfc810
Damon Oehlman <damon.oehlman@sidelab.com>
2012-07-10 20:17:02 +1000

    Test structure reorg

--- test/alt.js
+++ test/alt.js
@@ -1,5 +1,6 @@
 var testcli = require('testcli')(__dirname);
-
+    
 describe('altJS tests', function() {
-    it('should be able to generate coffee-script dist without traspilation', testcli('simple-coffee'));
+    it('should be able to generate coffee-script dist without traspilation', testcli('altjs-coffee'));    
+    it('should be able to transpile coffee-script to javascript', testcli('altjs-coffee2js'));
 });
--- test/build.js
+++ test/build.js
@@ -1,6 +1,6 @@
 var testcli = require('testcli')(__dirname);
 
 describe('build tests', function() {
-    it('should be able to perform a simple build', testcli('simple-build'));
-    it('should be able to perform a simple build that has a remote dependency', testcli('simple-webdep'));
+    it('should be able to perform a simple build', testcli('build-simple'));
+    it('should be able to perform a simple build that has a remote dependency', testcli('build-webdep'));
 });
