dddb012ccb6852754bb034467ac5f46e2f7e6ef1
Damon Oehlman <damon.oehlman@sidelab.com>
2012-07-11 09:34:40 +1000

    Made interleave transpile .coffee => .js by default

--- test/alt.js
+++ test/alt.js
@@ -1,6 +1,5 @@
 var testcli = require('testcli')(__dirname);
     
 describe('altJS tests', function() {
-    it('should be able to generate coffee-script dist without traspilation', testcli('altjs-coffee'));    
-    it('should be able to transpile coffee-script to javascript', testcli('altjs-coffee2js'));
+    it('should be able to convert coffee-script to js', testcli('altjs-coffee'));    
 });
