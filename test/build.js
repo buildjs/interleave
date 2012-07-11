346347a96004b5f12fc2db4efa380ea373bbd269
Damon Oehlman <damon.oehlman@sidelab.com>
2012-07-11 15:53:06 +1000

    Default to `src/*.js` when no paths are specified for build command

--- test/build.js
+++ test/build.js
@@ -3,4 +3,5 @@ var testcli = require('testcli')(__dirname);
 describe('build tests', function() {
     it('should be able to perform a simple build', testcli('build-simple'));
     it('should be able to perform a simple build that has a remote dependency', testcli('build-webdep'));
+    it('should automatically assume that "src/*.js" is requested when not specified', testcli('build-autoglob'));
 });
