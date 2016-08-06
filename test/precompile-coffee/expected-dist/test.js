61846cf5d47a5128f4cc1988425e8ff5edf71910
Damon Oehlman <damon.oehlman@gmail.com>
2016-08-06 20:10:35 +1000

    upgrade coffee script and test assets

--- test/precompile-coffee/expected-dist/test.js
+++ test/precompile-coffee/expected-dist/test.js
@@ -12,7 +12,7 @@
 }(this, function () {
     (function() {
       var cubes, list, math, num, number, opposite, race, square,
-        __slice = [].slice;
+        slice = [].slice;
     
       number = 42;
     
@@ -38,7 +38,7 @@
     
       race = function() {
         var runners, winner;
-        winner = arguments[0], runners = 2 <= arguments.length ? __slice.call(arguments, 1) : [];
+        winner = arguments[0], runners = 2 <= arguments.length ? slice.call(arguments, 1) : [];
         return print(winner, runners);
       };
     
@@ -47,13 +47,13 @@
       }
     
       cubes = (function() {
-        var _i, _len, _results;
-        _results = [];
-        for (_i = 0, _len = list.length; _i < _len; _i++) {
-          num = list[_i];
-          _results.push(math.cube(num));
+        var i, len, results;
+        results = [];
+        for (i = 0, len = list.length; i < len; i++) {
+          num = list[i];
+          results.push(math.cube(num));
         }
-        return _results;
+        return results;
       })();
     
     }).call(this);
